9b5457c17d73e159dbb3b63dbc2edd0dae66afe9
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-15 21:06:07 +0100

    Initial canvas setup and movement

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * -movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.up * -movementSpeed * Time.deltaTime);
+        }
+    }
+}
