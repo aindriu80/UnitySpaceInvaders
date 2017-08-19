554411fa8eae77db2e6b4ada701a347d386561f8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-19 10:30:56 +0100

    Enemy Ship Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,14 +7,47 @@ public class EnemyController : MonoBehaviour
 
     public float speed;
     public float changeTimer;
+    private float maxTimer;
+    private bool directionSwitch;
+
+    private Rigidbody rig;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    maxTimer = changeTimer;
+	    rig = GetComponent<Rigidbody>();
+	    directionSwitch = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switchTimer();
+		Movement();
 	}
+
+    void Movement()
+    {
+        if (directionSwitch)
+        {
+            rig.velocity = new Vector3(speed * Time.deltaTime, -speed * Time.deltaTime, 0);
+        }
+        else
+        {
+            rig.velocity = new Vector3(-speed * Time.deltaTime, -speed * Time.deltaTime, 0);
+        }
+    }
+
+    void switchTimer()
+    {
+        changeTimer -= Time.deltaTime;
+        if (changeTimer < 0)
+        {
+            
+                if (directionSwitch) directionSwitch = false;
+                else directionSwitch = true;
+                changeTimer = maxTimer;
+
+        }
+    }
 }
