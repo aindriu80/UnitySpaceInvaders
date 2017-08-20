7998aa652c38d2beb8feb1aa7a74e923f87ac2d2
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-20 11:50:26 +0100

    Random Enemy Generation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,25 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public MapLimits Limits;
     public GameObject enemy1;
     public GameObject enemy2;
     public GameObject enemy3;
+    public MapLimits Limits;
+    private float maxSpawnTimer;
+    public float spawnTimer;
 
+    // Use this for initialization
+    private void Start()
+    {
+        SpawnEnemy();
+        spawnTimer = maxSpawnTimer;
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-	    Instantiate(enemy1,
-	        new Vector3(Random.Range(Limits.minimumX, Limits.maximumX), 
-            Random.Range(Limits.minimumY, Limits.maximumY), 0), enemy1.transform.rotation);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            SpawnEnemy();
+            spawnTimer = maxSpawnTimer;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        var randomNumber = Random.Range(0, 2);
+        switch (randomNumber)
+        {
+            case 0:
+            {
+                Instantiate(enemy1,
+                    new Vector3(Random.Range(Limits.minimumX, Limits.maximumX),
+                        Random.Range(Limits.minimumY, Limits.maximumY), 0), enemy1.transform.rotation);
+            }
+                break;
+
+            case 1:
+            {
+                Instantiate(enemy2,
+                    new Vector3(Random.Range(Limits.minimumX, Limits.maximumX),
+                        Random.Range(Limits.minimumY, Limits.maximumY), 0), enemy1.transform.rotation);
+            }
+                break;
+
+            case 2:
+            {
+                Instantiate(enemy3,
+                    new Vector3(Random.Range(Limits.minimumX, Limits.maximumX),
+                        Random.Range(Limits.minimumY, Limits.maximumY), 0), enemy1.transform.rotation);
+            }
+                break;
+
+            default:
+            {
+                Instantiate(enemy1,
+                    new Vector3(Random.Range(Limits.minimumX, Limits.maximumX),
+                        Random.Range(Limits.minimumY, Limits.maximumY), 0), enemy1.transform.rotation);
+                break;
+            }
+        }
+    }
 }
